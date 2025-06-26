7c79326b6149ee9035bb58e948549b10bf4f1253
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-26 19:57:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
+# === main.tf ===
 provider "aws" {
   region = var.aws_region
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
 resource "aws_s3_bucket" "documents" {
@@ -7,24 +12,52 @@ resource "aws_s3_bucket" "documents" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
+  name = "lambda_exec_role_${random_id.suffix.hex}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Effect = "Allow",
-      Sid    = ""
-    }]
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "null_resource" "zip_parser" {
+  provisioner "local-exec" {
+    command = "zip -j lambda/parser_lambda.zip lambda/parser_lambda.py"
+  }
+}
+
+data "archive_file" "parser_lambda" {
+  type        = "zip"
+  source_file = "lambda/parser_lambda.py"
+  output_path = "lambda/parser_lambda.zip"
+  depends_on  = [null_resource.zip_parser]
+}
+
+resource "null_resource" "zip_search" {
+  provisioner "local-exec" {
+    command = "zip -j lambda/search_lambda.zip lambda/search_lambda.py"
+  }
+}
+
+data "archive_file" "search_lambda" {
+  type        = "zip"
+  source_file = "lambda/search_lambda.py"
+  output_path = "lambda/search_lambda.zip"
+  depends_on  = [null_resource.zip_search]
 }
 
 resource "aws_lambda_function" "parser" {
@@ -36,9 +69,9 @@ resource "aws_lambda_function" "parser" {
   source_code_hash = data.archive_file.parser_lambda.output_base64sha256
   environment {
     variables = {
-      DB_HOST     = aws_db_instance.pgvector.address
-      DB_USER     = var.db_user
-      DB_PASSWORD = var.db_password
+      DB_HOST        = aws_db_instance.pgvector.address
+      DB_USER        = var.db_user
+      DB_PASSWORD    = var.db_password
       OPENAI_API_KEY = var.openai_api_key
     }
   }
@@ -53,9 +86,9 @@ resource "aws_lambda_function" "search" {
   source_code_hash = data.archive_file.search_lambda.output_base64sha256
   environment {
     variables = {
-      DB_HOST     = aws_db_instance.pgvector.address
-      DB_USER     = var.db_user
-      DB_PASSWORD = var.db_password
+      DB_HOST        = aws_db_instance.pgvector.address
+      DB_USER        = var.db_user
+      DB_PASSWORD    = var.db_password
       OPENAI_API_KEY = var.openai_api_key
     }
   }
@@ -64,11 +97,13 @@ resource "aws_lambda_function" "search" {
 resource "aws_db_instance" "pgvector" {
   allocated_storage    = 20
   engine               = "postgres"
-  engine_version       = "13.4"
+  engine_version       = "15.4"
   instance_class       = "db.t3.micro"
+  db_name              = "vectorsearch"
   username             = var.db_user
   password             = var.db_password
   skip_final_snapshot  = true
+  publicly_accessible  = true
 }
 
 resource "aws_apigatewayv2_api" "search_api" {
@@ -77,11 +112,11 @@ resource "aws_apigatewayv2_api" "search_api" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.search_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.search.invoke_arn
-  integration_method = "POST"
-  payload_format_version = "2.0"
+  api_id                  = aws_apigatewayv2_api.search_api.id
+  integration_type        = "AWS_PROXY"
+  integration_uri         = aws_lambda_function.search.invoke_arn
+  integration_method      = "POST"
+  payload_format_version  = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "default_route" {
@@ -96,29 +131,14 @@ resource "aws_apigatewayv2_stage" "default" {
   auto_deploy = true
 }
 
-# ZIP LAMBDA SOURCE CODE USING TERRAFORM
-resource "null_resource" "zip_parser" {
-  provisioner "local-exec" {
-    command = "zip -j parser_lambda.zip ../lambda/parser_lambda.py"
-  }
+output "s3_bucket" {
+  value = aws_s3_bucket.documents.bucket
 }
 
-data "archive_file" "parser_lambda" {
-  type        = "zip"
-  source_file = "../lambda/parser_lambda.py"
-  output_path = "parser_lambda.zip"
-  depends_on  = [null_resource.zip_parser]
+output "db_endpoint" {
+  value = aws_db_instance.pgvector.address
 }
 
-resource "null_resource" "zip_search" {
-  provisioner "local-exec" {
-    command = "zip -j search_lambda.zip ../lambda/search_lambda.py"
-  }
-}
-
-data "archive_file" "search_lambda" {
-  type        = "zip"
-  source_file = "../lambda/search_lambda.py"
-  output_path = "search_lambda.zip"
-  depends_on  = [null_resource.zip_search]
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.search_api.api_endpoint
 }
