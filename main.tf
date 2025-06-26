72ebf81d5f30970b8b1314b5922ea27a2cde4f41
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-26 20:00:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -130,15 +130,3 @@ resource "aws_apigatewayv2_stage" "default" {
   name        = "$default"
   auto_deploy = true
 }
-
-output "s3_bucket" {
-  value = aws_s3_bucket.documents.bucket
-}
-
-output "db_endpoint" {
-  value = aws_db_instance.pgvector.address
-}
-
-output "api_endpoint" {
-  value = aws_apigatewayv2_api.search_api.api_endpoint
-}
