c4429577ca78695ed05fe50dc451dab6eae6a9bc
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-26 20:23:59 +0530

    Update search_lambda.py

--- lambda/search_lambda.py
+++ lambda/search_lambda.py
@@ -1,30 +1,26 @@
-import os, json, psycopg2
-from openai import OpenAIEmbeddings
-
-db_conn = psycopg2.connect(
-    host=os.environ['DB_HOST'],
-    user=os.environ['DB_USER'],
-    password=os.environ['DB_PASSWORD'],
-    dbname='vectorsearch'
-)
-
-def embed(query):
-    from openai import OpenAI
-    openai.api_key = os.environ['OPENAI_API_KEY']
-    return OpenAIEmbeddings().embed_query(query)
+import os
+import json
+import psycopg2
+from openai import OpenAI
 
 def lambda_handler(event, context):
-    body = json.loads(event["body"])
-    query = body['query']
-    vector = embed(query)
+    query = json.loads(event["body"])["query"]
+    client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+    embedding = client.embeddings.create(input=query, model="text-embedding-ada-002").data[0].embedding
 
-    cur = db_conn.cursor()
-    cur.execute("""
-        SELECT text FROM chunks ORDER BY embedding <-> %s LIMIT 5
-    """, (vector,))
-    results = cur.fetchall()
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        dbname='vectorsearch'
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT content FROM documents ORDER BY embedding <-> %s LIMIT 3", (embedding,))
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
 
     return {
         "statusCode": 200,
-        "body": json.dumps([r[0] for r in results])
+        "body": json.dumps({"results": [r[0] for r in rows]})
     }
